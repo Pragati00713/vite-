e2ebdee178addedc93ec2e455482419bdc29d193
Pragati00713 <kushwahapragati713@gmail.com>
2023-09-02 11:20:18 +0530

    simple landing page

---
+++ src/Cards.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <>
+    {/* kya bol rhe the */}
+    <div className='flex h-auto w-full flex-wrap p-5 bg-slate-300  gap-10 justify-center items-center '>
+ <div className='flex h-[400px]  bg-black text-white flex-col'>
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhwi-Bx9ma5Fidv66cyLEwJvjJgne169xXVQ&usqp=CAU" alt="" />
+     <div className=' text-center flex items-center flex-col justify-center '>
+         <h1 className='text-2xl'>cute wallpaper</h1> 
+         <h1 >Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, rerum!</h1>
+     </div>
+ </div>
+ <div className='flex h-[400px]  bg-black text-white  flex-col' >
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhwi-Bx9ma5Fidv66cyLEwJvjJgne169xXVQ&usqp=CAU" alt="" />
+     <div className=' text-center flex items-center flex-col justify-center ' >
+         <h1 className='text-2xl'>cute wallpaper</h1> 
+         <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, rerum!</h1>
+     </div>
+ </div>
+ <div className='flex h-[400px]  bg-black text-white   flex-col'>
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhwi-Bx9ma5Fidv66cyLEwJvjJgne169xXVQ&usqp=CAU" alt="" />
+     <div className=' text-center flex items-center flex-col justify-center '>
+         <h1 className='text-2xl'>cute wallpaper</h1> 
+         <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, rerum!</h1>
+     </div>
+ </div>
+ <div className='flex h-[400px]  bg-black text-white   flex-col'>
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhwi-Bx9ma5Fidv66cyLEwJvjJgne169xXVQ&usqp=CAU" alt="" />
+     <div className=' text-center flex items-center flex-col justify-center '>
+         <h1  className='text-2xl'>cute wallpaper</h1> 
+         <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, rerum!</h1>
+     </div>
+ </div>
+    </div>
+    
+    
+    <footer className='flex text-2xl h-auto w-full bg-black text-white justify-center items-center'>
+        <ul className=''>
+            <p>Copyright@pragati@123</p>
+            <p>thank you for visit my page</p>
+        </ul>
+    </footer>
+    
+    </>
+  )
+}
+
+export default Cards
---
+++ src/Pragati.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Pragati = () => {
+  return (
+    <>
+    {/* <h1 className='bg-slate-500  text-2xl text-red-300 text-center py-4 '>Hello</h1>
+    <h2 className='bg-slate-500  text-2xl text-red-300 text-center py-4 '>vishnu</h2> */}
+    <nav className='flex bg-black h-[80px] w-full justify-between text-white' >   
+<ul className='flex  items-center text-xl  gap-10'>
+     <li ><a href="#">Home</a></li> 
+     <li><a href="#">Service</a></li> 
+     <li><a href="#">About</a></li> 
+     <li><a href="#">Contact</a></li> 
+     <li><a href="#">Help</a></li> 
+</ul>
+
+    </nav>  
+
+    </>
+  )
+}
+
+export default Pragati
